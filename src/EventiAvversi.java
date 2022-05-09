665b04474d8832bbfbafe9916f4db1809eb1f9fa
cricci3 <cricci3@studenti.uninsubria.it>
2022-05-09 09:48:35 +0200

    Initial commit

---
+++ src/EventiAvversi.java
@@ -0,0 +1,20 @@
+public class EventiAvversi {
+    private CittadiniRegistrati cittadino;
+    private int valoreMalDiTesta;
+    private int valoreFebbre;
+    private int valoreDolori;
+    private int valoreLinfoadenopatia;
+    private int valoreTachicardia;
+    private int valoreCrisiI;
+
+    EventiAvversi(CittadiniRegistrati cittadino, int testa, int febbre, int dolori, int linfoadenopatia, int tachicardia, int crisi){
+        this.cittadino=cittadino;
+        this.valoreMalDiTesta=testa;
+        this.valoreFebbre=febbre;
+        this.valoreDolori=dolori;
+        this.valoreLinfoadenopatia=linfoadenopatia;
+        this.valoreTachicardia=tachicardia;
+        this.valoreCrisiI=crisi;
+
+    }
+}
---
+++ src/TestDB.java
@@ -0,0 +1,17 @@
+import java.sql.*;
+
+public class TestDB {
+
+    public static void main(String[] args) throws SQLException {
+        Database db = Database.getInstance();
+        DataTables dt = new DataTables();
+
+        ResultSet centriVaccinali = db.submitQuery("SELECT * FROM CentriVaccinali");
+        System.out.println(centriVaccinali);
+        dt.handleCentriVaccinaliSet(centriVaccinali);
+
+        for(CentriVaccinali cv: dt.getCentriVaccinaliTable()) { //per ogni studente S contento in ...
+            System.out.println(cv);
+        }
+    }
+}
