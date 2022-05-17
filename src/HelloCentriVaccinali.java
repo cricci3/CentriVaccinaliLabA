81f1e3836cd354ed5046d2218110a087240a1df8
Alessandro Macrina <alessandro.macrina1997@gmail.com>
2022-05-17 10:25:21 +0200

    Aggiunta schermata di accesso

--- src/HelloCentriVaccinali.java
+++ src/HelloCentriVaccinali.java
@@ -1,29 +1,32 @@
 import javax.swing.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class HelloCentriVaccinali {
-    private JPanel panel1;
-    private JButton btnOperatore;
-    private JButton btnCittadino;
+    private JPanel Panel1;
+    private JButton btn_cittadino;
+    private JButton btn_operatore;
+    private JLabel label;
 
     public HelloCentriVaccinali() {
-        btnOperatore.addActionListener(new ActionListener() {
+
+        btn_cittadino.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "premuto Operatore");
+                JOptionPane.showMessageDialog(null, "Hello World!");
             }
         });
-        btnCittadino.addActionListener(new ActionListener() {
+        btn_operatore.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "premuto Cittadino");
+                JOptionPane.showMessageDialog(null, "Hello World 2!");
             }
         });
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("HelloCentriVaccinali");
-        frame.setContentPane(new HelloCentriVaccinali().panel1);
+        frame.setContentPane(new HelloCentriVaccinali().Panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
